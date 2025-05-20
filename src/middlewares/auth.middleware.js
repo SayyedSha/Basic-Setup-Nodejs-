b66a7e8e9dd39e31f529f3b0cad8575257b6398c
Shahanawaz <shahanawazsayyed0@gmail.com>
2025-05-20 22:57:08 +0530

    This is a basic setup of backend of production level

---
+++ src/middlewares/auth.middleware.js
@@ -0,0 +1,40 @@
+import { ApiError } from "../utils/ApiError.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
+import jwt from "jsonwebtoken"
+import { User } from "../models/user.model.js";
+
+export const verifyJWT = asyncHandler(async(req, _, next) => {
+    try {
+        const token = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ", "")
+        
+        // console.log(token);
+        if (!token) {
+            throw new ApiError(401, "Unauthorized request")
+        }
+    
+        const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET)
+    
+        const user = await User.findById(decodedToken?._id).select("-password -refreshToken")
+    
+        if (!user) {
+            
+            throw new ApiError(401, "Invalid Access Token")
+        }
+    
+        req.user = user;
+        next()
+    } catch (error) {
+        throw new ApiError(401, error?.message || "Invalid access token")
+    }
+    
+})
+
+
+export const checkRole = (roles) => async (req, res, next) => {
+  
+  //retrieve employee info from DB
+  const user = await User.findOne({ _id:req.user._id });
+  !roles.includes(user.role)
+    ? res.status(401).json("Sorry you do not have access to this route")
+    : next();
+};
